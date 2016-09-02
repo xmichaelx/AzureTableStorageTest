a08dd29767582bf54f72a754f44c51535a449a2f
xmichaelx <michal.drzal@gmail.com>
2016-09-02 23:27:51 +0200

    Added querying test

--- AzureTableStorageTest/Program.cs
+++ AzureTableStorageTest/Program.cs
@@ -12,13 +12,15 @@ namespace AzureTableStorageTest
             var accountName = args[0];
             var accountKey = args[1];
             var size = int.Parse(args[2]);
-            const int batchSize = 100; 
+            const int batchSize = 100;
+            const int queryBatchSize = 1000;
+            const int days = 30;
             using (var wrapper = new Wrapper(accountName, accountKey))
             {
                 // generate test data entities
                 Stopwatch timer = new Stopwatch();
                 timer.Start();
-                var testData = TestDataGenerator.GenerateData(DateTimeOffset.UtcNow.AddDays(-30), DateTimeOffset.UtcNow, size, 300).Select(x => new TableItem(x)).ToList();
+                var testData = TestDataGenerator.GenerateData(DateTimeOffset.UtcNow.AddDays(-days), DateTimeOffset.UtcNow, size, 50).Select(x => new TableItem(x)).ToList();
                 timer.Stop();
                 Console.WriteLine($"Creating {size} items elapsed {timer.Elapsed} s");
                 timer.Restart();
@@ -26,8 +28,14 @@ namespace AzureTableStorageTest
                 timer.Stop();
                 Console.WriteLine($"Data upload elapsed {timer.Elapsed} s - {timer.ElapsedMilliseconds / (size/ batchSize)} ms per batch request");
                 // querying
+                for (int i = 0; i < days; i++)
+                {
 
-
+                    timer.Restart();
+                    var items = wrapper.GetItems("primaryid", DateTimeOffset.UtcNow.AddDays(-(i+1)), DateTimeOffset.UtcNow.AddDays(-i));
+                    timer.Stop();
+                    Console.WriteLine($"Data query for day {i}'s back retrieved: {items.Count} items and elapsed {timer.Elapsed} s - {timer.ElapsedMilliseconds / (((double)items.Count) / queryBatchSize)} ms per batch request");
+                }
 
                 // deleting
                 timer.Restart();
